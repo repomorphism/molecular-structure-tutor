702566bbac1ffcfbe68f4e83db5145eff24c0cc8
Hsing Liu <liu.hsing@gmail.com>
2020-04-26 14:33:52 -0400

    Refactor out carbon and hydrogen atoms

--- src/components/AddHydrogenButton.vue
+++ src/components/AddHydrogenButton.vue
@@ -1,19 +1,7 @@
 <template>
   <button @click="setAddHydrogen">
     <span>
-      <svg
-        width="31"
-        height="31"
-        viewBox="0 0 31 31"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <ellipse cx="15.4243" cy="15.2727" rx="14.8485" ry="15.1515" fill="#7DAEE9" />
-        <path
-          d="M8.82623 22.3638V6.52979H12.1026V12.5669H18.2901V6.52979H21.5772V22.3638H18.2901V15.2954H12.1026V22.3638H8.82623Z"
-          fill="white"
-        />
-      </svg>
+      <hydrogen-atom />
       <span>Hydrogen</span>
     </span>
   </button>
@@ -22,9 +10,11 @@
 <script>
 import { mapActions } from "vuex";
 import { ClickMode } from "../store/constants";
+import HydrogenAtom from "./HydrogenAtom.vue";
 
 export default {
   name: "AddHydrogenButton",
+  components: { HydrogenAtom },
   methods: {
     ...mapActions(["setClickMode"]),
     setAddHydrogen() {
---
+++ src/components/HydrogenAtom.vue
@@ -0,0 +1,21 @@
+<template>
+  <svg width="31" height="31" viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <ellipse cx="15.4243" cy="15.2727" rx="14.8485" ry="15.1515" fill="#7DAEE9" />
+    <path
+      d="M8.82623 22.3638V6.52979H12.1026V12.5669H18.2901V6.52979H21.5772V22.3638H18.2901V15.2954H12.1026V22.3638H8.82623Z"
+      fill="white"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: "HydrogenAtom"
+};
+</script>
+
+<style scoped>
+header {
+  height: 58px;
+}
+</style>
